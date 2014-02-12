f220785e9d7fd20053b6c387a54ee2f3a92b1854
Jordan McCall <jmccall707@gmail.com>
2014-02-11 16:57:29 -0800

    added our STABLE version of the KinectPlay project that we used for the 2/10 demo

---
+++ StableGloveOVRKinect/Assets/CameraSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+	
+	public Camera camera1;
+	public Camera camera2;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			if (camera1.enabled == true) {
+				camera1.enabled = false;
+				camera2.enabled = true;
+			} else {
+				camera1.enabled = true;
+				camera2.enabled = false;
+			}
+		}
+		
+	}
+}
