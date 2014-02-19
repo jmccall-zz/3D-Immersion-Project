417819b0eadbac7ca47aabe00f6b6b7897f1e4aa
Conor Kaminer <conorkaminer@gmail.com>
2014-02-18 17:46:18 -0800

    Added the CalibrateGlove scene

---
+++ MainGame/Assets/Scripts/RecordAndMove.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var moving : boolean;
+private var trans : Transform;
+static var amount : float = 0.25;
+
+function Start () {
+	moving = false;
+	trans = transform;
+}
+
+function Update () {
+	if(Input.GetKeyDown("space") && trans.position.x % 20 == 0) {
+		Record();
+		moving = true;
+	}
+	if (moving) {
+		trans.position.x = trans.position.x + amount;
+		if (trans.position.x % 20 == 0) moving = false;
+	}
+	
+}
+
+function Record() {
+	
+}
