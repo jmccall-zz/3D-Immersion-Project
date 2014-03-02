eba228b6b8e2d36f6658ba9f95e73260d6f45afa
Conor Kaminer <conorkaminer@gmail.com>
2014-03-01 17:39:44 -0800

    Created Login GUI, first step towards main menu

---
+++ MainGame/Assets/Scripts/Menu.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var textFieldString = "Name";
+
+function OnGUI() {
+	textFieldString = GUI.TextField (Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), textFieldString);
+	if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 20, 150, 20), "Login")) {
+		
+	}
+}
