2461b7a874b9fe55712dc488cbd18def59609d79
Conor Kaminer <conorkaminer@gmail.com>
2014-03-01 22:00:31 -0800

    Added scene changes

--- MainGame/Assets/Scripts/Menu.js
+++ MainGame/Assets/Scripts/Menu.js
@@ -5,6 +5,6 @@ var textFieldString = "Name";
 function OnGUI() {
 	textFieldString = GUI.TextField (Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), textFieldString);
 	if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 20, 150, 20), "Login")) {
-		
+		Application.LoadLevel("CalibrateGlove");
 	}
 }
