c423e87ef41ac15aadc3c6b5fac16c5ab8356a47
Conor Kaminer <conorkaminer@gmail.com>
2014-03-03 12:40:09 -0800

    Added pause menu to all scenes

--- MainGame/Assets/Scripts/Menu.js
+++ MainGame/Assets/Scripts/Menu.js
@@ -1,10 +1,36 @@
 ﻿#pragma strict
 
 var textFieldString = "Name";
+var MenuState = false;
+
+
+function Update() {
+	if (Input.GetKeyDown(KeyCode.Escape)) {
+		MenuState = !MenuState;
+		if (MenuState) 
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+}
 
 function OnGUI() {
-	textFieldString = GUI.TextField (Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), textFieldString);
-	if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 20, 150, 20), "Login")) {
-		Application.LoadLevel("CalibrateGlove");
+	if (MenuState) {
+		//textFieldString = GUI.TextField (Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), textFieldString);
+		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 - 60, 150, 20), "Resume")) {
+			MenuState = !MenuState;
+		}
+		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 - 30, 150, 20), "Load Login Scene")) {
+			Application.LoadLevel("Login");
+		}
+		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2, 150, 20), "Load Calibrate")) {
+			Application.LoadLevel("CalibrateGlove");
+		}
+		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 30 , 150, 20), "Load Ball Game")) {
+			Application.LoadLevel("FirstScene");
+		}
+		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 60, 150, 20), "Exit")) {
+			Application.Quit();
+		}
 	}
 }
