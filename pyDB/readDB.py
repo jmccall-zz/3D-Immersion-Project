c80b3efaee2f8dfb832394957b3da82bf79c623c
Conor Kaminer <conorkaminer@gmail.com>
2014-04-16 16:53:55 -0700

    Fixed position in reachback Added GUI python file for reading database Made a class in readDB.py for easy reading of database data

--- pyDB/readDB.py
+++ pyDB/readDB.py
@@ -3,13 +3,28 @@
 import sqlite3
 import random
 import matplotlib.pyplot as plt
-import dateutil
 
-#db = "TestDB"
-db = "C:\\Users\\jmccall\\Documents\\3D-Immersion-Project\\MainGame\\RehabStats.sqdb"
-con = sqlite3.connect(db)
-print("Database Opened")
 
+
+class dbReader():
+        db = "C:\\Users\\jmccall\\Documents\\3D-Immersion-Project\\MainGame\\RehabStats.sqdb"
+#       db = "TestDB"
+
+        # connection Object
+        con = []
+        
+        def __init__(self):
+                #Initialize Database Connection
+                self.con = sqlite3.connect(self.db)
+                print("Database Opened")
+
+        def readNames(self):
+                names=[]
+                cur = self.con.execute("SELECT first_name, last_name from UserProfiles")
+                for row in cur:
+                        names.append(row[1] + ',' + row[0])
+
+                return names
 
 #con.execute('''CREATE TABLE STROKEDATA
 #(ID INT PRIMARY KEY NOT NULL,
@@ -18,15 +33,20 @@ print("Database Opened")
 #for i in range (0,1024):
 #   con.execute("INSERT INTO STROKEDATA (ID, DATA) VALUES(" + str(i) + "," + str(random.randint(0,1024)) + ");")
 
-x = []
-y = []
+#x = []
+#y = []
 
-cur = con.cursor()
-cur = con.execute("SELECT p_id, created_dtm from UserProfiles")
-for row in cur:
-	x.append(row[0])
-	y.append(row[1])
+#cur = con.cursor()
+#cur = con.execute("SELECT p_id, created_dtm from UserProfiles")
+#for row in cur:
+#	x.append(row[0])
+#	y.append(row[1])
 	
-print (x)
-print (y)
+#print (x)
+#print (y)
+
+if __name__ == '__main__':
+        thedb = dbReader()
+        print(thedb.readNames())
+
 
