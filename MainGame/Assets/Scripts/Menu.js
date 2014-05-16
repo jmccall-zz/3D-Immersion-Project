7a36549bbc7b6b140a10930a904b45955b76350f
Jordan McCall <jmccall707@gmail.com>
2014-05-16 16:45:13 -0700

    menu.js only loads a scene switch scene (zigfu bugfix)

--- MainGame/Assets/Scripts/Menu.js
+++ MainGame/Assets/Scripts/Menu.js
@@ -6,43 +6,6 @@ var MenuState = false;
 
 function Update() {
 	if (Input.GetKeyDown(KeyCode.Escape)) {
-		MenuState = !MenuState;
-		if (MenuState) 
-			Time.timeScale = 0;
-		else
-			Time.timeScale = 1;
-	}
-	if(Input.GetKeyDown(KeyCode.R)) {
-		Application.LoadLevel(Application.loadedLevel);
-	}
-}
-
-function OnGUI() {
-	if (MenuState) {
-		//textFieldString = GUI.TextField (Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), textFieldString);
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 - 60, 150, 20), "Resume")) {
-			MenuState = !MenuState;
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 - 30, 150, 20), "Load Login Scene")) {
-			Application.LoadLevel("Login");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2, 150, 20), "Load Calibrate")) {
-			Application.LoadLevel("CalibrateGlove");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 30 , 150, 20), "Load Ball Game")) {
-			Application.LoadLevel("FirstScene");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 60 , 150, 20), "Load Reach Test")) {
-			Application.LoadLevel("ReachBack");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 90 , 150, 20), "Load Grip Test")) {
-			Application.LoadLevel("GripStrength");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 150, Screen.height/2 + 120 , 300, 20), "Upright Shoulder Abduction Measurement")) {
-			Application.LoadLevel("ShoulderROM");
-		}
-		if (GUI.Button (Rect (Screen.width/2 - 75, Screen.height/2 + 150, 150, 20), "Exit")) {
-			Application.Quit();
-		}
+		Application.LoadLevel("SceneSwitch");
 	}
 }
