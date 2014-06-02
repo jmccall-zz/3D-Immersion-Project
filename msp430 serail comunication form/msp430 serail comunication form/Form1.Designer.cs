2b5eeaf0f62588b904b0b39fe41c702bd5524124
kevinpatxi <kevin.patxi@gmail.com>
2014-06-01 20:02:24 -0700

    Updated serial comm. program

--- msp430 serail comunication form/msp430 serail comunication form/Form1.Designer.cs
+++ msp430 serail comunication form/msp430 serail comunication form/Form1.Designer.cs
@@ -500,7 +500,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 20;
+            this.timer1.Interval = 60;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // groupBox2
